f1fbe8c1adc51d76831a0e45462f92c8b6bd7b77
Berik <eraliev237@gmail.com>
2023-06-16 17:44:36 +0600

    14-15.06.2023

---
+++ Перепишите_if_в_questionsymb.js
@@ -0,0 +1,10 @@
+// let result;
+
+// if (a + b < 4) {
+//     result = 'Мало';
+// } else {
+//     result = 'Много';
+// }
+
+'use strict';
+let result = (a + b < 4) ? 'Мало' : 'Много';
---
+++ Придумайте_правильные_имена.js
@@ -0,0 +1,3 @@
+'use strict';
+let ourPlanetName;
+let currentUserName;
